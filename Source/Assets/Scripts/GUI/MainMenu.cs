6d3f95d5f144e0ebfa1fa8baa3474f80500dd18c
VegK <eal.prog@gmail.com>
2015-11-25 23:28:48 +1100

    Create main menu

---
+++ Source/Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	#region Properties
+	#region Public
+	
+	#endregion
+	#region Private
+
+	#endregion
+	#endregion
+
+	#region Methods
+	#region Public
+	public void OnClickStart()
+	{
+		Application.LoadLevel("Game");
+	}
+	#endregion
+	#region Private
+
+	#endregion
+	#endregion
+}
