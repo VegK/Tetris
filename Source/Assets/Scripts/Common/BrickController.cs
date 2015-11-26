23ab88f83b2de4eb4ca918c391b46464423bcb5a
VegK <eal.prog@gmail.com>
2015-11-26 17:22:30 +1100

    Brick fall

--- Source/Assets/Scripts/Common/BrickController.cs
+++ Source/Assets/Scripts/Common/BrickController.cs
@@ -6,6 +6,8 @@ public class BrickController : MonoBehaviour
 	#region Properties
 	#region Public
 	public BrickType TypeBrick;
+
+	public float SpeedFall { get; set; }
 	#endregion
 	#region Private
 
@@ -19,12 +21,19 @@ public class BrickController : MonoBehaviour
 	#region Private
 	private void Start()
 	{
-		
+
 	}
-	
+
 	private void Update()
 	{
-		
+		Fall();
+	}
+
+	private void Fall()
+	{
+		var speed = Vector3.down;
+		speed.y *= SpeedFall * Time.deltaTime;
+		transform.Translate(speed);
 	}
 	#endregion
 	#endregion
