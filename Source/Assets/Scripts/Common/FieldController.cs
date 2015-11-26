10ac0ffb51187a70f02ffc8849b6859a51efa017
VegK <eal.prog@gmail.com>
2015-11-26 18:34:03 +1100

    Gizmos for field, collider bottom.

--- Source/Assets/Scripts/Common/FieldController.cs
+++ Source/Assets/Scripts/Common/FieldController.cs
@@ -8,19 +8,34 @@ public class FieldController : MonoBehaviour
 	#region Properties
 	#region Public
 	public Vector2 Size = new Vector2(10, 20);
+	public float SpeedOnOneLevel = 2f;
 	public Transform StartPointBricks;
 	[Header("Bricks")]
-	public GameObject BrickI;
-	public GameObject BrickJ;
-	public GameObject BrickL;
-	public GameObject BrickO;
-	public GameObject BrickS;
-	public GameObject BrickT;
-	public GameObject BrickZ;
+	public BrickController PrefabBrickI;
+	public BrickController PrefabBrickJ;
+	public BrickController PrefabBrickL;
+	public BrickController PrefabBrickO;
+	public BrickController PrefabBrickS;
+	public BrickController PrefabBrickT;
+	public BrickController PrefabBrickZ;
+
+	public int Level
+	{
+		get
+		{
+			return _level;
+		}
+		set
+		{
+			_level = value;
+		}
+	}
 	#endregion
 	#region Private
+	private int _level;
 	private bool[,] _field;
 	private BrickType _nextBrick;
+	private BrickController _currentBrick;
 	#endregion
 	#endregion
 
@@ -31,13 +46,30 @@ public class FieldController : MonoBehaviour
 	#region Private
 	private void Awake()
 	{
+		var box = gameObject.AddComponent<BoxCollider>();
+		box.isTrigger = true;
+
+		var boxSize = box.size;
+		boxSize.x = Size.x;
+		box.size = boxSize;
+
+		var boxCenter = box.center;
+		boxCenter.x = Size.x / 2 - 0.5f;
+		boxCenter.y = -1f;
+		box.center = boxCenter;
+
 		_field = new bool[(int)Size.x, (int)Size.y];
 
+		if (Level == 0)
+			Level = 1;
 	}
 
 	private void Start()
 	{
-		
+		_nextBrick = GetNextBrick();
+		CreateBrick();
+
+		_nextBrick = GetNextBrick();
 	}
 	
 	private void Update()
@@ -62,32 +94,60 @@ public class FieldController : MonoBehaviour
 		return (BrickType)bricks.GetValue(UnityEngine.Random.Range(0, bricks.Length));
 	}
 
-	private GameObject GetBrick(BrickType type)
+	private BrickController GetBrick(BrickType type)
 	{
 		switch (type)
 		{
 			default:
 			case BrickType.I:
-				return BrickI;
+				return PrefabBrickI;
 			case BrickType.J:
-				return BrickJ;
+				return PrefabBrickJ;
 			case BrickType.L:
-				return BrickL;
+				return PrefabBrickL;
 			case BrickType.O:
-				return BrickO;
+				return PrefabBrickO;
 			case BrickType.S:
-				return BrickS;
+				return PrefabBrickS;
 			case BrickType.T:
-				return BrickT;
+				return PrefabBrickT;
 			case BrickType.Z:
-				return BrickZ;
+				return PrefabBrickZ;
 		}
 	}
 
 	private void CreateBrick()
 	{
 		var brick = Instantiate(GetBrick(_nextBrick));
+		brick.transform.SetParent(transform);
 		brick.transform.position = StartPointBricks.position;
+		brick.SpeedFall = Level * SpeedOnOneLevel;
+		_currentBrick = brick;
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Brick")
+		{
+			_currentBrick.SpeedFall = 0;
+
+		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+
+		for (int x = 0; x < (int)Size.x; x++)
+		{
+			for (int y = 0; y < (int)Size.y; y++)
+			{
+				var pos = transform.position;
+				pos.x += x;
+				pos.y += y;
+				Gizmos.DrawWireCube(pos, Vector2.one);
+			}
+		}
 	}
 	#endregion
 	#endregion
